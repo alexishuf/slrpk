916e550c63b4c2decb56691c6bbbf55446e2af6d
Alexis Armin Huf <alexishuf@gmail.com>
2018-07-07 10:32:52 -0300

    Added --deref-value, --append and --append-sep to set-field-expr

--- src/main/java/com/github/alexishuf/slrpk/commands/SetFieldExpr.java
+++ src/main/java/com/github/alexishuf/slrpk/commands/SetFieldExpr.java
@@ -9,12 +9,23 @@ import org.kohsuke.args4j.Option;
 import javax.annotation.Nonnull;
 import java.io.File;
 import java.util.List;
+import java.util.Objects;
 
 public class SetFieldExpr extends SetField {
     @Argument(usage = "Splirk expression yielding the Works to have --value assigned.")
     private String[] exprTerms;
     @Option(name = "--expr-file", forbids = {"--stdin"}, usage = "Load expression from given file")
     private File exprFile;
+    @Option(name = "--deref-value", usage = "Treat --value as a field name. The value in that " +
+            "field for the first matched Work in the given expression will be used as the " +
+            "actual new value")
+    private boolean derefValue = false;
+    @Option(name = "--append", usage = "Instead of discarding the old value, append the new value " +
+            "to the existing one, optionally adding with --append-sep as separator")
+    private boolean append = false;
+    @Option(name = "--append-sep", usage = "Override default separator used with " +
+            "--append (default is \"\")")
+    private String appendSep = "";
     @Option(name = "--stdin", forbids = {"--expr-file"},
             usage = "Load expression from standard input")
     private boolean stdin = false;
@@ -40,6 +51,11 @@ public class SetFieldExpr extends SetField {
 
     @Override
     protected PredicateMatch applyPredicate(@Nonnull Work work) {
-        return fromBib.stream().anyMatch(work::matches) ? new PredicateMatch(work, value) : null;
+        Work match = fromBib.stream().filter(work::matches).findFirst().orElse(null);
+        if (match == null) return null;
+        String value = derefValue ? match.get(this.value) : this.value;
+        if (append)
+            value = work.get(field) + appendSep + value;
+        return new PredicateMatch(work, value != null ? value : defaultValue);
     }
 }
